b8d76fb6a89766624b82d6c771355d45383cc419
senortrashcan <hnl9324@gmail.com>
2024-09-08 18:58:27 -0500

    no admin in url

--- src/index.js
+++ src/index.js
@@ -37,11 +37,11 @@ root.render(
     <BackgroundColorWrapper>
       <BrowserRouter>
         <Routes>
-          <Route path="/admin/*" element={<AdminLayout />} />
+          <Route path="/*" element={<AdminLayout />} />
           <Route path="/rtl/*" element={<RTLLayout />} />
           <Route
             path="*"
-            element={<Navigate to="/admin/dashboard" replace />}
+            element={<Navigate to="/dashboard" replace />}
           />
         </Routes>
       </BrowserRouter>

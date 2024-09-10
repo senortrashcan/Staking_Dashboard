2fe9ba94d4f644793652453370f89761c347b256
senortrashcan <hnl9324@gmail.com>
2024-09-09 21:21:12 -0500

    Update Admin.js

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -80,7 +80,7 @@ function Admin(props) {
   };
   const getRoutes = (routes) => {
     return routes.map((prop, key) => {
-      if (prop.layout === "/admin") {
+      if (prop.layout === "/") {
         return (
           <Route path={prop.path} element={prop.component} key={key} exact />
         );
@@ -121,7 +121,7 @@ function Admin(props) {
                 {getRoutes(routes)}
                 <Route
                   path="/"
-                  element={<Navigate to="/admin/dashboard" replace />}
+                  element={<Navigate to="/dashboard" replace />}
                 />
               </Routes>
               {

cf29b0c868daacc3dd115ce6a15278a333e04b99
senortrashcan <hnl9324@gmail.com>
2024-09-09 21:50:13 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ root.render(
           <Route path="/rtl/*" element={<RTLLayout />} />
           <Route
             path="*"
-            element={<Navigate to="/admin/dashboard" replace />}
+            element={<Navigate to="/dashboard" replace />}
           />
         </Routes>
       </BrowserRouter>

d6984e2798f0fb8d73245c1798560181ce350303
senortrashcan <hnl9324@gmail.com>
2024-09-15 21:28:47 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,6 +26,7 @@ import "assets/scss/black-dashboard-react.scss";
 import "assets/demo/demo.css";
 import "assets/css/nucleo-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import "@cryptofonts/cryptofont/cryptofont.css";
 
 import ThemeContextWrapper from "./components/ThemeWrapper/ThemeWrapper";
 import BackgroundColorWrapper from "./components/BackgroundColorWrapper/BackgroundColorWrapper";

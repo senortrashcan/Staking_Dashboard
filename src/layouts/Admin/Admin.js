f63eb7a8736bbb6270b48b3b28878a23a4857a61
senortrashcan <hnl9324@gmail.com>
2024-09-16 19:27:17 -0500

    Update Admin.js

--- src/layouts/Admin/Admin.js
+++ src/layouts/Admin/Admin.js
@@ -118,7 +118,6 @@ function Admin(props) {
                 toggleSidebar={toggleSidebar}
                 sidebarOpened={sidebarOpened}
               />
-              <Divider><Chip label="Staking" size="large" /></Divider>
               <Routes>
                 {getRoutes(routes)}
                 <Route

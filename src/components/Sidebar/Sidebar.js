115367ee5e856a3048dfe62f2ce7366ed005416c
senortrashcan <hnl9324@gmail.com>
2024-09-16 20:33:02 -0500

    Update Sidebar.js

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -131,7 +131,7 @@ function Sidebar(props) {
             return (
               <React.Fragment key={key}>
                 {showDivider && (
-                  <div style={{ padding: '16px 0' }}>
+                  <div style={{ paddingtop: '16px 0', paddingbottom: '10px 0' }}>
                   <Divider sx={{ color: '#ffffff' }} textAlign="center">
                     Staking
                   </Divider>

56e6510cdcabcf18f373ade192b69d67d757020d
senortrashcan <hnl9324@gmail.com>
2025-08-26 21:27:04 -0600

    fucking hell

--- src/views/Dashboard.js
+++ src/views/Dashboard.js
@@ -111,7 +111,86 @@ function Dashboard(props) {
             </div>
           </Col>
         </Row>
-        
+        <Row>
+          <Col xs="12">
+            <Card className="card-chart">
+              <CardHeader>
+                <Row>
+                  <Col className="text-left" sm="6">
+                    <h5 className="card-category">Total Shipments</h5>
+                    <CardTitle tag="h2">Performance</CardTitle>
+                  </Col>
+                  <Col sm="6">
+                    <ButtonGroup
+                      className="btn-group-toggle float-right"
+                      data-toggle="buttons"
+                    >
+                      <Button
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data1",
+                        })}
+                        color="info"
+                        id="0"
+                        size="sm"
+                        onClick={() => setBgChartData("data1")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Accounts
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-single-02" />
+                        </span>
+                      </Button>
+                      <Button
+                        color="info"
+                        id="1"
+                        size="sm"
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data2",
+                        })}
+                        onClick={() => setBgChartData("data2")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Purchases
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-gift-2" />
+                        </span>
+                      </Button>
+                      <Button
+                        color="info"
+                        id="2"
+                        size="sm"
+                        tag="label"
+                        className={classNames("btn-simple", {
+                          active: bigChartData === "data3",
+                        })}
+                        onClick={() => setBgChartData("data3")}
+                      >
+                        <span className="d-none d-sm-block d-md-block d-lg-block d-xl-block">
+                          Sessions
+                        </span>
+                        <span className="d-block d-sm-none">
+                          <i className="tim-icons icon-tap-02" />
+                        </span>
+                      </Button>
+                    </ButtonGroup>
+                  </Col>
+                </Row>
+              </CardHeader>
+              <CardBody>
+                <div className="chart-area">
+                  <Line
+                    data={chartExample1[bigChartData]}
+                    options={chartExample1.options}
+                  />
+                </div>
+              </CardBody>
+            </Card>
+          </Col>
+        </Row>
        
       </div>
     </>
--- src/views/solana.js
+++ src/views/solana.js
@@ -16,11 +16,31 @@
 
 */
 import React, { useEffect } from "react";
+import 'assets/css/stakingkiwi.css'; // Import the CSS file
 // reactstrap components
 import { Card, CardHeader, CardBody, Row, Col } from "reactstrap";
 
 
 function Solana() {
+  useEffect(() => {
+    // Create script element
+    const script = document.createElement('script');
+    script.src = "https://widget.staking.kiwi/js/widget.min.js";
+    script.async = true;
+    script.onload = () => {
+      // Script has loaded and is ready to use
+      console.log('Staking widget script loaded');
+    };
+
+    // Append script to the body
+    document.body.appendChild(script);
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []); // Empty dependency array ensures this effect runs only once after initial render
+
   return (
     <React.Fragment>
       <div className="content">
@@ -31,11 +51,23 @@ function Solana() {
                 <div className="text-center">
                   <h5 className="title">Solana Staking</h5>
                   <p className="category">
-                    *This Section is UNDER CONSTUCTION!
+                    *Please note we are still in the process of integrating our own interface. Your assets are safe.
                   </p>
                 </div>
               </CardHeader>
               <CardBody className="all-icons">
+                <div className="iframe-container">
+                  <iframe
+                    id="staking-kiwi-widget-0"
+                    scrolling="no"
+                    frameBorder="0"
+                    allowTransparency="true"
+                    allowFullScreen
+                    className="staking-widget-iframe"
+                    title="Staking Kiwi widget"
+                    src="https://widget.staking.kiwi/?validator=&theme=dark"
+                  ></iframe>
+                </div>
               </CardBody>
             </Card>
           </Col>
